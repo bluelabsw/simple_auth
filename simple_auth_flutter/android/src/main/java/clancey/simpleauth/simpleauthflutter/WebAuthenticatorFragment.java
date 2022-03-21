3df81e04051a5ce29b14c8f769b5ed6d20ab3ecc
Mauritz Mollentze <mauritz.mollentze@bluelab.com>
2022-03-22 11:47:59 +1300

    Add WebAuthenticatorFragment to replace activity
    - On certain devices when the WebAuthenticatorActivity is presented onDetachedFromEngine will be called and the channels will be destroyed.

---
+++ simple_auth_flutter/android/src/main/java/clancey/simpleauth/simpleauthflutter/WebAuthenticatorFragment.java
@@ -0,0 +1,101 @@
+package clancey.simpleauth.simpleauthflutter;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.CookieManager;
+import android.webkit.URLUtil;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class WebAuthenticatorFragment extends DialogFragment {
+    private static final String AUTHENTICATOR_KEY = "AUTHENTICATOR_KEY";
+
+    WebView webview;
+    public static String UserAgent = "";
+
+    public static HashMap<String,WebAuthenticator> States = new HashMap<>();
+
+    public static WebAuthenticatorFragment newInstance(WebAuthenticator authenticator) {
+        String stateKey = UUID.randomUUID().toString();
+        WebAuthenticatorFragment.States.put(stateKey, authenticator);
+        WebAuthenticatorFragment fragment = new WebAuthenticatorFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(AUTHENTICATOR_KEY, stateKey);
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
+
+    WebAuthenticator authenticator;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(STYLE_NO_FRAME, android.R.style.Theme_Holo_Light);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        authenticator = States.get(getArguments().getString(AUTHENTICATOR_KEY));
+
+        webview = new WebView(getContext());
+
+        authenticator.addListener(authenticator.new CompleteNotifier(){
+            @Override
+            public void onComplete() {
+                webview.stopLoading();
+                dismiss();
+            }
+        });
+
+        WebSettings settings = webview.getSettings();
+        CookieManager.getInstance().removeAllCookies(null);
+        CookieManager.getInstance().flush();
+        if(UserAgent != null && !UserAgent.isEmpty())
+        {
+            settings.setUserAgentString(UserAgent);
+            settings.setLoadWithOverviewMode(true);
+        }
+        settings.setJavaScriptEnabled(true);
+        webview.setWebViewClient(new Client(this));
+
+        if(savedInstanceState != null)
+        {
+            webview.restoreState(savedInstanceState);
+        }
+
+        webview.loadUrl(authenticator.initialUrl);
+        return webview;
+    }
+
+    class Client extends WebViewClient
+    {
+        private WebAuthenticatorFragment activity;
+
+        Client(WebAuthenticatorFragment activity)
+        {
+            this.activity = activity;
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            String url = request.getUrl().toString();
+            if (!url.isEmpty()) {
+                if (URLUtil.isNetworkUrl(url)) {
+                    return false;
+                } else if (url.startsWith(activity.authenticator.redirectUrl.toString())){
+                    activity.authenticator.checkUrl(url,false);
+                }
+            }
+            return true;
+        }
+    }
+}
